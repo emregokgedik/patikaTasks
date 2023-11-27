60a40cd8853ac960ea309120bcfbc4ae0dafbd73
Emre Gökgedik <emregokgedik2@gmail.com>
2023-11-27 20:47:45 +0000

    Removing to-do items function added

--- Front-End Course/1.Beginner Level Front-End Course/4.JS/todolist/js/script.js
+++ Front-End Course/1.Beginner Level Front-End Course/4.JS/todolist/js/script.js
@@ -1,12 +1,24 @@
+/*var buton=document.querySelectorAll(".close").forEach(function(element){
+    element.addEventListener("click",function(){
+        element.parentElement.remove();
+    })
+})*/
+
 function newElement(){
     let task=document.querySelector("#task")
     if(task.value!=""){
         let liste=document.querySelector("ul")
         let li=document.createElement("li"); 
         li.textContent=task.value;
+        li.innerHTML+=`<span type="button" class="close" data-dismiss="alert" aria-label="Close" style="background-color:none;">
+        <span aria-hidden="true">&times;</span>`
         liste.appendChild(li)
+        var buton=document.querySelectorAll(".close").forEach(function(element){
+            element.addEventListener("click",function(){
+                element.parentElement.remove();
+            })
+        })
         $(".success").toast('show');
-
     }else{
         $(".error").toast('show');
     }
