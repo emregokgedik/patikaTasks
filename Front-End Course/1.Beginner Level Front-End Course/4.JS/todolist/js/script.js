0dd3a64d31b671db73355d8174cffdfc70a714ba
Emre Gökgedik <emregokgedik2@gmail.com>
2023-11-27 21:12:51 +0000

    Added selecting to-do items feature

--- Front-End Course/1.Beginner Level Front-End Course/4.JS/todolist/js/script.js
+++ Front-End Course/1.Beginner Level Front-End Course/4.JS/todolist/js/script.js
@@ -13,6 +13,17 @@ function newElement(){
         li.innerHTML+=`<span type="button" class="close" data-dismiss="alert" aria-label="Close" style="background-color:none;">
         <span aria-hidden="true">&times;</span>`
         liste.appendChild(li)
+        
+        var eleman=document.querySelectorAll("#list>li").forEach(function(element){
+            element.addEventListener("click",function(){
+                if(element.classList.contains('checked')){
+                    element.classList.remove('checked')
+                }else{
+                element.classList.add('checked')
+                }
+            })
+        })
+
         var buton=document.querySelectorAll(".close").forEach(function(element){
             element.addEventListener("click",function(){
                 element.parentElement.remove();
