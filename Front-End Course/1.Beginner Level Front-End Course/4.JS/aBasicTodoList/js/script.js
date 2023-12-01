417d5bda7dc6c7ef9c66e81aaff08472f7b9dfca
Emre Gökgedik <emregokgedik2@gmail.com>
2023-12-01 11:09:14 +0000

    Localstorage added&edit function developed

--- Front-End Course/1.Beginner Level Front-End Course/4.JS/aBasicTodoList/js/script.js
+++ Front-End Course/1.Beginner Level Front-End Course/4.JS/aBasicTodoList/js/script.js
@@ -3,7 +3,7 @@ const taskList=document.getElementById("list"); //assign ul element(entire list)
 
 taskList.addEventListener("click",function(e){
     if(e.target.tagName==="I"){
-        editTodo();
+        editTodo(e);
     }else if(e.target.tagName==="BUTTON"){
         deleteTodo(e.target);
     }else{
@@ -18,12 +18,12 @@ function addTodo(){
     }else{
         let li=document.createElement("li");
         li.classList.add("list-group-item","d-flex","justify-content-between","align-items-start");
-        li.innerHTML=`<div class="me-auto"><input class="form-check-input" type="checkbox">
-                ${taskInput.value} </div><i onclick="editTodo(this)" class="fa-solid fa-pen me-1"></i><button type="button" class="btn-close "  aria-label="Close"></button>
+        li.innerHTML=`<div class="me-auto"><input class="form-check-input" type="checkbox"> ${taskInput.value}</div><i class="fa-solid fa-pen me-1"></i><button type="button" class="btn-close "  aria-label="Close"></button>
             `
         taskList.appendChild(li);
         alert("Görev başarıyla eklendi");
         taskInput.value="";
+        saveData();
     }
 }
 
@@ -36,9 +36,13 @@ addTodo();
 function deleteTodo(button){
     var listItem = button.closest('.list-group-item');
     listItem.parentNode.removeChild(listItem);
+    saveData();
 }
-function editTodo() {
-    console.log("edit clicked")
+function editTodo(e) {
+    let newItem=prompt("Yeni değeri girin",e.target.parentNode.querySelector(".me-auto").textContent);
+    if(!newItem==="null"||!newItem=="")
+    e.target.parentNode.querySelector(".me-auto").innerHTML=`<input class="form-check-input" type="checkbox"> ${newItem}`
+    saveData();
 }
 
 function selectTodo(e) {
@@ -50,4 +54,14 @@ function selectTodo(e) {
         checked.classList.add("text-decoration-line-through");
         e.target.closest("li").querySelector(".form-check-input").checked=true;
     }
+    
+    saveData();
 }
+
+function saveData(){
+    localStorage.setItem("data",taskList.innerHTML);
+}
+function showData(){
+    taskList.innerHTML=localStorage.getItem("data");
+}
+showData();
