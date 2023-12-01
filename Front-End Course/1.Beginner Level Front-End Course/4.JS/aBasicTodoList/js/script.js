542ce48d62131f9eb4b876184fb3b3d264281df3
Emre Gökgedik <emregokgedik2@gmail.com>
2023-12-01 00:37:05 +0000

    The main page has designed

---
+++ Front-End Course/1.Beginner Level Front-End Course/4.JS/aBasicTodoList/js/script.js
@@ -0,0 +1,15 @@
+/*
+İşlevler
+-Yeni görev ekleme
+-Varolan görevi silme
+-Varolan görev üzerinde değişiklik yapma
+*/
+function addTodo(task){
+    
+}
+function deleteTodo(task){
+    
+}
+function editTodo(oldTask, newTask) {
+    // Belirli bir görevi değiştirme kodları
+}
