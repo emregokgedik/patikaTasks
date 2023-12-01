9a5ee36569db72aa92e8c012bd19551f64d88c9f
Emre Gökgedik <emregokgedik2@gmail.com>
2023-12-01 00:53:43 +0000

    Adding new to-do javascript function developed

--- Front-End Course/1.Beginner Level Front-End Course/4.JS/aBasicTodoList/js/script.js
+++ Front-End Course/1.Beginner Level Front-End Course/4.JS/aBasicTodoList/js/script.js
@@ -1,15 +1,24 @@
-/*
-İşlevler
--Yeni görev ekleme
--Varolan görevi silme
--Varolan görev üzerinde değişiklik yapma
-*/
-function addTodo(task){
+const taskInput=document.getElementById("taskInput");
+const taskList=document.getElementById("list");
+
+
+function addTodo(){
+    if(taskInput.value===""){
+        alert("Bir görev girmelisin!");
+    }else{
+        let li=document.createElement("li");
+        li.classList.add("list-group-item");
+        li.innerHTML=`<input class="form-check-input" type="checkbox">
+                ${taskInput.value} <button type="button" class="btn-close " aria-label="Close"></button>
+            `
+        taskList.appendChild(li);
+        alert("Görev başarıyla eklendi");
+        taskInput.value="";
+    }
+}
+function deleteTodo(){
     
 }
-function deleteTodo(task){
+function editTodo() {
     
-}
-function editTodo(oldTask, newTask) {
-    // Belirli bir görevi değiştirme kodları
 }
