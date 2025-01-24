cb54b3b82e2ed810e448895b6258ce4049332b57
emregokgedik <emregokgedik2@gmail.com>
2025-01-24 23:16:16 +0300

    Ordered 3 different fetch proceses

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/learningNode/callback.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/learningNode/callback.js
@@ -19,5 +19,16 @@ import fetch from "node-fetch"
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then((data) => data.json()
-.then(users => console.log(users))
+.then(users => {
+    console.log("Users Loaded",users)
+
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(data => data.json())
+    .then(posts => console.log("Post 1 Loaded",posts))
+
+    fetch("https://jsonplaceholder.typicode.com/posts/2")
+    .then(data => data.json())
+    .then(posts => console.log("Post 2 Loaded",posts))
+})
 );
