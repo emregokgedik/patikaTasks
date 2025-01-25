2b8e83755aaef559e5f834a44ad467968a25e266
emregokgedik <emregokgedik2@gmail.com>
2025-01-25 10:46:50 +0300

    Refactor: Replace fetch with async/await for improved readability and added post data fetching.

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/learningNode/callback.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/learningNode/callback.js
@@ -17,18 +17,29 @@ import fetch from "node-fetch"
 //     console.log("Hello")
 // });
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((data) => data.json()
-.then(users => {
-    console.log("Users Loaded",users)
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((data) => data.json()
+// .then(users => {
+//     console.log("Users Loaded",users)
 
 
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(data => data.json())
-    .then(posts => console.log("Post 1 Loaded",posts))
+//     fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then(data => data.json())
+//     .then(posts => console.log("Post 1 Loaded",posts))
 
-    fetch("https://jsonplaceholder.typicode.com/posts/2")
-    .then(data => data.json())
-    .then(posts => console.log("Post 2 Loaded",posts))
-})
-);
+//     fetch("https://jsonplaceholder.typicode.com/posts/2")
+//     .then(data => data.json())
+//     .then(posts => console.log("Post 2 Loaded",posts))
+// })
+// );
+
+async function getData(){
+    const users = await (await fetch("https://jsonplaceholder.typicode.com/users")).json()
+    const post1 = await (await fetch("https://jsonplaceholder.typicode.com/posts/1")).json()
+    const post2 = await (await fetch("https://jsonplaceholder.typicode.com/posts/2")).json()
+    console.log("users",users);
+    console.log("post1",post1);
+    console.log("post2",post2);
+}
+
+getData();
