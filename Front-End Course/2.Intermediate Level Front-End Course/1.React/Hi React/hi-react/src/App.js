84ec3685e0593d535644de922cae1d69c17252ec
emregokgedik <emregokgedik2@gmail.com>
2025-01-28 23:55:13 +0300

    feat: Add user greeting with conditional rendering
    -Introduced name and surname variables for user details.
    
    -Added isLoggedIn variable to simulate user authentication.
    
    -Implemented conditional rendering to display a personalized greeting if the user is logged in, or a login prompt otherwise.

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/App.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/Header.js'
 
+const name = "Emre"
+const surname = "Gokgedik"
+const isLoggedIn = true;
+
 function App(){
   return(
     <div>
-      <h1>Hello React</h1>
+      <h1>
+      {isLoggedIn
+      ? `Hello ${name} ${surname}`:"Please login."}
+      </h1>
       <Header />
     </div>
   )
