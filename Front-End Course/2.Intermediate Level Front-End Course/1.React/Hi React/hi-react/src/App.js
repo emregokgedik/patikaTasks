6c851db3b0ea5ff2d9e3ba085f23b0c8d6f10947
emregokgedik <emregokgedik2@gmail.com>
2025-01-28 23:59:15 +0300

    refactor: Combine name and surname into a single variable
    - Created `fullName` variable to store the user's full name.
    
    - Simplified the template string by using the `fullName` variable.

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/App.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Header from './components/Header.js'
 
-const name = "Emre"
-const surname = "Gokgedik"
+const name = "Emre";
+const surname = "Gokgedik";
 const isLoggedIn = true;
+const fullName = `${name} ${surname}`;
+
+
 
 function App(){
   return(
     <div>
       <h1>
       {isLoggedIn
-      ? `Hello ${name} ${surname}`:"Please login."}
+      ? `Hello ${fullName}`:"Please login."}
       </h1>
       <Header />
     </div>
