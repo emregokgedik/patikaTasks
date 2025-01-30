a4c63a7f6300d73c1428b3c0f38f4aafe2347183
emregokgedik <emregokgedik2@gmail.com>
2025-01-30 21:44:16 +0300

    Refactor User component: Add PropTypes validation & improve safety
    - Added PropTypes to validate component props and ensure correct data types.
    - Removed unnecessary console.log statement.
    - Added a null check for the `friends` array to prevent potential errors.

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/components/User.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Hi React/hi-react/src/components/User.js
@@ -1,12 +1,13 @@
+import PropTypes from "prop-types";
+
 function User({name, surname, age, isLoggedIn, friends}){
-    console.log(name, surname, age, isLoggedIn, friends);
     return (
         <>
         <h1>
             {isLoggedIn ? `Hello ${name} ${surname}! (age: ${age})` : "Please login."}
         </h1>
         <h2>Friends</h2>
-        {
+        {friends &&
             friends.map((friend,index)=>
             <div key={friend.id}>{friend.name}</div>
         )
@@ -14,5 +15,13 @@ function User({name, surname, age, isLoggedIn, friends}){
         </>
     )
 }
+
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.number.isRequired,
+    friends: PropTypes.array
+  };
 
 export default User;
