be322d31001fec5693fbbaf6dc8bf9b991d2ee42
emregokgedik <emregokgedik2@gmail.com>
2025-02-01 23:39:10 +0300

    Add: Friends state and display functionality, 'add friend' button

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/react-states/src/App.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/react-states/src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 function App() {
   const [name, setName] = useState("Emre");
   const [age, setAge] = useState(18);
+  const [friends, setFriends] = useState (['Ahmet','Murat']);
   return (
     <div className="App">
         <h1>Hi {name} !</h1>
         <h1>Age: {age}</h1>
         <button onClick={()=>setName("Ahmet")}>Set Name</button>
         <button onClick={()=>setAge(19)}>Set Age</button>
+        <hr/>
+        <h2>Friends</h2>
+        {
+          friends.map((friend,index) =>
+          (<div key={index}>{friend}</div>
+          ))
+        }
+        <button onClick={()=>setFriends(...friends,'Ayse')}>Add New Friend</button>
     </div>
   );
 }
