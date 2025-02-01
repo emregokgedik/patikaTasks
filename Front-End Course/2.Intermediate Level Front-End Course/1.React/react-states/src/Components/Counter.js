564803179bcba41a7d515cc30c85afb25a601672
emregokgedik <emregokgedik2@gmail.com>
2025-02-01 23:56:35 +0300

    Refactor: Extract increase and decrease functions in Counter.js

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/react-states/src/Components/Counter.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/react-states/src/Components/Counter.js
@@ -2,11 +2,17 @@ import { useState } from 'react'
 
 function Counter() {
     const [count,setCount] = useState(0)
+    const increase = () => {
+        setCount(count+1)
+    }
+    const decrease = () => {
+        setCount(count-1)
+    }
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={()=>setCount(count-1)}>Decrease</button>
-      <button onClick={()=>setCount(count+1)}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={increase}>Increase</button>
     </div>
   )
 }
