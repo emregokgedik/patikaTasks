2e161a3c7e82155e3739bbc6af5aec0b226cc11f
emregokgedik <emregokgedik2@gmail.com>
2025-02-03 23:37:24 +0300

    Extract: All functions extracted from App.js to Counter.js

---
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Lifecycle in React/lifecycle-in-react/src/components/Counter.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function Counter() {
+  const [number,setNumber] = useState(0)
+  const [name,setName] = useState("Emre")
+
+  useEffect(()=>{
+    console.log("Component mounted");
+  },[])
+
+  useEffect(()=>{
+    console.log("Number state updated");
+  },[number])
+
+  useEffect(()=>{
+    console.log("Name state updated");
+  },[name])
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={()=>setNumber(number+1)}>Click</button>
+
+      <hr/>
+
+      <h1>{name}</h1>
+      <button onClick={()=>setName("Ahmet")}>Click</button>
+    </div>
+  );
+}
+
+export default Counter;
