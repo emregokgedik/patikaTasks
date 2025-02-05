eb9f75f12588969f606b82bca493160367da3013
emregokgedik <emregokgedik2@gmail.com>
2025-02-05 19:39:50 +0300

    Feat: Show/hide button added for the counter

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Lifecycle in React/lifecycle-in-react/src/App.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Lifecycle in React/lifecycle-in-react/src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 function App() {
-
+  const [isVisible,setIsVisible]=useState(true);
   return (<div className='App'>
-    <Counter />
+    {isVisible&&<Counter />}
+    <button onClick={()=>setIsVisible(!isVisible)}>Show/Hide</button>
   </div>);
 }
 
