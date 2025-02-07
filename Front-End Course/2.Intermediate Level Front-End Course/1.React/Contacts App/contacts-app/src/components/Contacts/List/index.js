63a12ee0548e2b357b7d0f301e3b1e29a2cde2d7
emregokgedik <emregokgedik2@gmail.com>
2025-02-07 21:34:03 +0300

    Feat: -Added an input to filter contacts

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Contacts App/contacts-app/src/components/Contacts/List/index.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Contacts App/contacts-app/src/components/Contacts/List/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
+  const[filterText,setFilterText] = useState('');
+
+  const filtered=contacts.filter((item)=>{
+    return Object.keys(item).some((key)=>
+      item[key]
+      .toString().toLowerCase().includes(filterText.toLowerCase())
+    )
+  })
+
   return (
     <div>
+      <input placeholder='Filter Contact' value={filterText} onChange={(e)=>setFilterText(e.target.value)}/>
       <ul>
-        {contacts.map((contact,i)=>(
+        {filtered.map((contact,i)=>(
           <li key={i}>{contact.full_name}</li>
         ))}
       </ul>
--- Front-End Course/2.Intermediate Level Front-End Course/1.React/Contacts App/contacts-app/src/components/Contacts/index.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/Contacts App/contacts-app/src/components/Contacts/index.js
@@ -3,7 +3,17 @@ import List from './List'
 import Form from './Form'
 
 function Contacts() {
-  const [contacts,setContacts] = useState([]);
+  const [contacts,setContacts] = useState([{
+    full_name:'Emre',
+    phone_number:'123123'
+  },
+  {
+    full_name:'Ahmet',
+    phone_number:'123123'
+  },{
+    full_name:'Nazli',
+    phone_number:'123123'
+  },]);
   useEffect(()=>{
     console.log("Contacts log:",contacts);
   },[contacts])
