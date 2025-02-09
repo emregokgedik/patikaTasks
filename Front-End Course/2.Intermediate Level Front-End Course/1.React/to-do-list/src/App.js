ca2e86e44b1f2f7188ac4c870e329c4b31272541
emregokgedik <emregokgedik2@gmail.com>
2025-02-09 19:50:23 +0300

    Initialize: Replace default react files with task codes

--- Front-End Course/2.Intermediate Level Front-End Course/1.React/to-do-list/src/App.js
+++ Front-End Course/2.Intermediate Level Front-End Course/1.React/to-do-list/src/App.js
@@ -4,20 +4,74 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<section class="todoapp">
+	<header class="header">
+		<h1>todos</h1>
+		<form>
+			<input class="new-todo" placeholder="What needs to be done?" autoFocus />
+		</form>
+	</header>
+	
+	<section class="main">
+		<input class="toggle-all" type="checkbox" />
+		<label for="toggle-all">
+			Mark all as complete
+		</label>
+
+		<ul class="todo-list">
+			<li class="completed">
+				<div class="view">
+					<input class="toggle" type="checkbox" />
+					<label>Learn JavaScript</label>
+					<button class="destroy"></button>
+				</div>
+			</li>
+			<li>
+				<div class="view">
+					<input class="toggle" type="checkbox" />
+					<label>Learn React</label>
+					<button class="destroy"></button>
+				</div>
+			</li>
+			<li>
+				<div class="view">
+					<input class="toggle" type="checkbox" />
+					<label>Have a life!</label>
+					<button class="destroy"></button>
+				</div>
+			</li>
+		</ul>
+	</section>
+
+	<footer class="footer">
+		<span class="todo-count">
+			<strong>2</strong>
+			items left
+		</span>
+
+		<ul class="filters">
+			<li>
+				<a href="#/" class="selected">All</a>
+			</li>
+			<li>
+				<a href="#/">Active</a>
+			</li>
+			<li>
+				<a href="#/">Completed</a>
+			</li>
+		</ul>
+
+		<button class="clear-completed">
+			Clear completed
+		</button>
+	</footer>
+</section>
+
+<footer class="info">
+	<p>Click to edit a todo</p>
+	<p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>
     </div>
   );
 }
